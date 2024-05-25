f8c20267ef6b8fca5e1c27fcdaede0031834b1c1
Ram-india <bhv.ramece@gmail.com>
2024-05-25 17:00:22 +0530

    dynamic data --date and sum example

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Hello from "./components/Hello";
  
  //Component:App
  const App = () => {
+    const a =20;
+    const b = 20;
+    const now =new Date();
   return (
     <div>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-      <Hello/>
+      date:{now.toString()}
+      <p>value of a is {a}b value is {b} and total is {a+b}</p>
       <Hello/>
     </div>
   )
