8e435d753c22a92cfff40a0e420c9cf66caeffd5
Ram-india <bhv.ramece@gmail.com>
2024-05-25 18:09:35 +0530

    conditional rendering example-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-
 const App = () => {
-  const students = [
-    {id:1, name:"Ram", age:20},
-    {id:2, name:"Ramya", age:19},
-    {id:3, name:"Kumar", age:28},
-    {id:4, name:"Raman", age:20},
-  ]
 
-  const studentsList = []
-  for (let i = 0; i < students.length; i++){
-    studentsList.push(
-      <li key={students[i].id}>{students[i].name} {students[i].age}</li>
-    )
-  }
-  return (
-    <div>
-      <h1>Students</h1>
-      <ul>
-        {studentsList}
-      </ul>
-    </div>
-  )
+const isLogIn = true;
+if (isLogIn){
+  return <p>Welcome back!</p>
 }
-
+else{
+  return<p>Please Login</p>
+}
+}
 export default App
