3cdc190aa2950d2bed3d0f4f9a1484c2a81b88fc
Ram-india <bhv.ramece@gmail.com>
2024-05-27 12:02:08 +0530

    Increment, Decrement, Reset are created by using useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
  //Component:App
  const App = () => {
-  return <h1>Sri Angalamman</h1>
+    let[count, setCount] = useState(0);
+
+const handleIncrement = () =>{
+    setCount(count + 1)
+}
+const handleDecrement = () =>{
+    setCount(count - 1)
+}
+const handleReset = () =>{
+    setCount(count = 0)
+}
+
+  return (
+    <div>
+        <h1>counter:{count}</h1>
+        <button onClick={handleIncrement}>increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Reset</button>
+    </div>
+  )
  }
  export default App;
