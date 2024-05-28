cdaa4eaee5f9e5d566d873dfd1623fa6a6279605
Ram-india <bhv.ramece@gmail.com>
2024-05-28 10:32:43 +0530

    intial template

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -5,7 +5,7 @@ class Hello extends Component{
     render(){
         return(
             <div>
-                <h1>Hello {this.props.name}!</h1>
+                <h1>Hell {this.props.name}!</h1>
             </div>
         )
     }
