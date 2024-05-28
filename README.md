ba649d2f5191da44b3a486c108c11682d63e7368
Ram-india <bhv.ramece@gmail.com>
2024-05-28 12:11:21 +0530

    React Roter configration

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# Day 04
+# Day 06
 ## React- Day -4: React components
 - 24/05/2024 - Friday - 10:00 AM : 1:00 PM
 - Contents:
-- [ ]Components - In-depth
-- [ ]Component life cycle
-- [ ]Stateful and stateless components
-- [ ]Reusable components 
-- [ ]Passing dynamic data to component
-- [ ]Introduction to Hook
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import Hello from './components/Hello';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter ([
+   { path: "/",
+    element: <h1>Hello!</h1>
+    },
+    {
+        path:"/about",
+        element: <h1>about</h1>
+    },
+    {
+        path:"/products",
+        element:<h1>Products</h1>
+    },
+    {
+        path:"/services",
+        element:<h1>Services</h1>
+    },
+    {
+        path:"/contact",
+        element:<h1>Contact us</h1>
+    }
+])
 
 const App = () => {
-  return (
-    <div>App
-        <Hello/>
-    </div>
+    //warp the app with the router
+  return (<RouterProvider router={router}/>
 
   )
 }
