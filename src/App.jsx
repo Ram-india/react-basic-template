30732e9673ba9906ef1c295a273597e0b5793ade
Ram-india <bhv.ramece@gmail.com>
2024-05-29 12:28:11 +0530

    Link Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+//create a router
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-
-// create a router
+import {Link} from "react-router-dom";
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-    children:[
-        {
-            path:"/",
-            element:<div><a href="/register">Register</a>
-            &nbsp;|&nbsp;<a href="/login">Login</a></div>,
-        },
-        {
-            path:"register",
-            element:<Register/>
-        },
-        {
-            path:"login",
-            element:<Login/>
-        }
-    ]
-
-  },
-  
-]);
+    {
+        path:"/",
+        element:<Home/>,
+        children:[
+            {
+                path:"/",
+                element:<div><Link to="/register">Register</Link>&nbsp;|&nbsp;<Link to="/login">Login</Link></div>
+            
+            },
+            {
+                path:"register",
+                element:<Register/>
+            },
+            {
+                path:"login",
+                element:<Login/>
+            }
+        ]
+    }
+])
 
 
-const App = () => {
-  // wrap the app with the RouterProvider
-  return <RouterProvider router={router} />;
+function App() {
+  return<RouterProvider router={router}/>
 }
+
 export default App;
-
-/*
-  Exercise: 
-
-  1. Create a new route for the path "/contact" with the element <h1>Contact Us</h1>
-  2. Create a new route for the path "/services" with the element <h1>Our Services</h1>
-  3. Create a new route for the path "/products" with the element <h1>Our Products</h1>
-*/
