a7220b75cecbe4cc6ef199c9c1e37d3c045f4c44
Ram-india <bhv.ramece@gmail.com>
2024-05-31 11:06:02 +0530

    recreated context as in seesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
- //Component:App
+import { createContext,useContext,useState } from "react"
+
+ //1.create a context to store the name
+ const NameContext = createContext();
+ 
+ const D = () =>{
+    //3.use the context in the component
+    const {name} = useContext(NameContext);
+    return <h1>Hello,{name}!</h1>
+}
+ const C = () =>{
+    return <D/>
+       
+ }
+ const B = () =>{
+    return <C/>
+ }
+ 
+ //2. Warp the component tree with the context provider
  const App = () => {
-  return <h1>Sri Angalamman</h1>
+    const [name, setName] = useState('Ram');
+  return(
+
+    <NameContext.Provider value={{name}}>
+        <B />
+    </NameContext.Provider>
+  ) 
  }
  export default App;
