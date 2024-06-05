7c4ad3281ec5ba496e4b8e0b3d8f343ca5e9a2b5
Ram-india <bhv.ramece@gmail.com>
2024-06-05 06:32:12 +0530

    focus created by using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { useRef } from "react";
+
  //Component:App
  const App = () => {
-  return <h1>Sri Angalamman</h1>
+    //create reffernce using useref
+    const inputRef = useRef(null);
+    const inputRef1 = useRef(null);
+   
+
+    const handleClick = () =>{
+       //console.log(inputRef.current.value   );
+       inputRef.current.focus();
+       
+      
+ 
+    }
+    const handleClick1 = () =>{
+        //console.log(inputRef.current.value   );
+        
+        inputRef1.current.focus();
+       
+  
+     }
+
+  return (
+    <>
+    <input
+    type="text"
+    
+    ref={inputRef}/>
+    <button onClick={handleClick}>Focus input</button>
+    <br/>
+    <br/>
+    <input
+    type="text"
+  
+    ref={inputRef1}/>
+    <button onClick={handleClick1}>Focus input</button>
+    </>
+  )
  }
  export default App;
